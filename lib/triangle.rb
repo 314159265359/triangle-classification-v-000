89c82130858543d9bee4e4cbefdee27ca3c62d6a
Karen G <kgraves0701@gmail.com>
2018-04-28 11:34:50 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,8 +13,6 @@ class Triangle
     if (@s1 != 0 && @s2 != 0 && @s3 != 0) && ((@s1+@s2>@s3) && (@s2+@s3>@s1) && (@s3+@s1>@s2)) == false || (@s1+@s2+@s3 == 0)
       begin
         raise TriangleError
-      # rescue TriangleError => error
-      #     puts error.message
       end
     end
 
@@ -33,7 +31,6 @@ class Triangle
   end
 
   class TriangleError < StandardError
-    # triangle error code
   end
 
 end
