c538735f86c88ac18deafc6f9fb61046a62b6e4b
Karen G <kgraves0701@gmail.com>
2018-04-28 11:36:30 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -26,7 +26,7 @@ class Triangle
 
   end
 
-  def valid?
+  def self.valid?
     (@s1 != 0 && @s2 != 0 && @s3 != 0) && ((@s1+@s2>@s3) && (@s2+@s3>@s1) && (@s3+@s1>@s2)) == false || (@s1+@s2+@s3 == 0)
   end
 
