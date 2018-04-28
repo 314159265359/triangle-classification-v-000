5d0a48ad70e0b930e768b623e87cb9f97df718c2
Karen G <kgraves0701@gmail.com>
2018-04-28 11:36:24 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -27,7 +27,7 @@ class Triangle
   end
 
   def valid?
-    (@s1 != 0 && @s2 != 0 && @s3 != 0) && ((@s1+@s2>@s3) && (@s2+@s3>@s1) && (@s3+@s1>@s2)) == false || (@s1+@s2+@s3 == 0)  
+    (@s1 != 0 && @s2 != 0 && @s3 != 0) && ((@s1+@s2>@s3) && (@s2+@s3>@s1) && (@s3+@s1>@s2)) == false || (@s1+@s2+@s3 == 0)
   end
 
   class TriangleError < StandardError
