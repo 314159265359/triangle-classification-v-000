f9a007b7ad2f4dd5fc3715eaddcb9175d0001295
Karen G <kgraves0701@gmail.com>
2018-04-28 11:32:17 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,8 @@ class Triangle
   end
 
   def kind
-    if valid? == false
+    # if valid? == false
+    if (@s1 != 0 && @s2 != 0 && @s3 != 0) && ((@s1+@s2>@s3) && (@s2+@s3>@s1) && (@s3+@s1>@s2)) == false
       begin
         raise TriangleError
       rescue TriangleError => error
